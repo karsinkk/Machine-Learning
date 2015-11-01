ff44189c336583dcd7fe37c69d3964681d34b055
karsinkk <karsinkk@gmail.com>
2015-11-01 15:41:52 +0530

    Initial Commit

--- Logistic Regression/Logistic Regression.py
+++ Logistic Regression/Logistic Regression.py
@@ -5,40 +5,24 @@ import pandas as pd
 import math
 import os
 
-
-
 CWD = os.getcwd()
 
 # Getting the Data
-path = CWD+"\Logistic Regression\Data\Training.csv";
+path = CWD + "\Logistic Regression\Data\Training.csv"
 ColNames = ['Sepal Length', 'Sepal Width', 'Petal Length', 'Petal Width', 'Class']
-train = pd.read_csv(path,names=ColNames)
+train = pd.read_csv(path, names=ColNames)
 Y = train.Class.tolist()
 X = np.genfromtxt(path, delimiter=',')
 X = X[:, :-1]
 
-theta = np.zeros(X.shape[1]+1)
-y1 = list(map(lambda x: int(x == "Iris-setosa"),Y))
+theta = np.zeros(X.shape[1] + 1)
+y1 = list(map(lambda x: int(x == "Iris-setosa"), Y))
 y1 = np.array(y1)
-y2 = list(map(lambda x: int(x == "Iris-versicolor"),Y))
+y2 = list(map(lambda x: int(x == "Iris-versicolor"), Y))
 y2 = np.array(y2)
-y3 = list(map(lambda x: int(x == "Iris-virginica"),Y))
+y3 = list(map(lambda x: int(x == "Iris-virginica"), Y))
 y3 = np.array(y3)
 
 
 def signum(x):
-    return 1/(1+math.exp(-x))
-
-print()
-
-
-
-
-
-
-
-
-
-
-
-
+    return 1 / (1 + math.exp(-x))
