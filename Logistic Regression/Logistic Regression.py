fb1e73aae59190b5e74f11d0296b33bf8c55d947
karsinkk <karsinkk@gmail.com>
2015-11-11 01:21:18 +0530

    Implemented Gradient Descent.

--- Logistic Regression/Logistic Regression.py
+++ Logistic Regression/Logistic Regression.py
@@ -21,9 +21,11 @@ X = np.matrix(X)
 y = np.hstack((np.matrix(list(map(lambda x: int(x == "Iris-setosa"), Y))).T, np.matrix(list(map(lambda x: int(x == "Iris-versicolor"), Y))).T,np.matrix(list(map(lambda x: int(x == "Iris-virginica"), Y))).T))
 theta = np.zeros((n, 3))
 lamb = 1
+alpha = 1
+iter = 100
 
 
-# Regularization Functions
+#  Logistic Regression Functions
 sigmoid = np.vectorize(lambda x: 1 / (1 + math.exp(-x)))
 
 log10 = np.vectorize(math.log10)
@@ -32,5 +34,23 @@ def cost(_x, _y, _theta, _lamb):
      j = -(1 / m) * (_y.T.dot(log10(sigmoid(_x.dot(_theta.T))).T) + ((1 - _y).T.dot(log10(sigmoid(1 - (_x.dot(_theta.T)))).T))) + (_lamb / 2 * m) * (_theta.dot(_theta.T))
      return j
 
-print(cost(X,y[:,0],theta[:,0],lamb))
+def descent(_x, _y, _theta, _lamb, _alpha, _iter):
+
+     _theta = np.matrix(_theta).T
+
+     for i in range(1,_iter + 1):
+          g = _theta
+          g[0,:] = g[0,:] - (_alpha / m) * (sigmoid(_x.dot(g)) - _y[:,0]).T.dot(_x[:,0])
+          g[1:,] = g[1:,] * (1 - (_alpha * _lamb / m)) - (_alpha / m) * (_x[:,1:]).T.dot(sigmoid(_x.dot(g)) - _y)
+          _theta = g
+     return _theta
+
+
+# Training(Calculating Theta using Gradient Descent)
+for k in range(0,y.shape[1]):
+
+     theta[:,k] = descent(X,y[:,k],theta[:,k],lamb,alpha,iter).T
+
+
+
 
