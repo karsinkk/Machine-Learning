862f46f404ebea860d7a6d79226004be787bb916
karsinkk <karsinkk@gmail.com>
2016-01-18 23:19:37 +0530

    Modified Regression.py

--- Pure Python/Regression.py
+++ Pure Python/Regression.py
@@ -12,7 +12,7 @@ import numpy as np
 class Regression(object):
 
     def __init__(self, aplha=.15, Lambda=.0001, epoch=100, X, Y, labels):
-        """ Initialize all the parameters"""
+        """ Initializing all the parameters"""
         X = np.c_[np.ones(m), X]
         X = np.matrix(X)
         self.X, self.Y = X, Y
@@ -20,44 +20,54 @@ class Regression(object):
         self.alpha, self.Lambda, self.epoch = alpha, Lambda, epoch
         self.theta = np.zeros((n, self.k))
         self.labels = labels
+        self.cost_epoch = []
 
     def cost(self):
         """ Cost Function """
-        j = -(1 / self.m) * (Y.T.dot(np.log10(sigmoid(X.dot(Theta.T))).T) +
-                    (
-                     (1 - Y).T.dot(np.log10(sigmoid(1 - (X.dot(Theta.T)))).T))
-                      ) + (Lambda / 2 * self.m) * (Theta.dot(Theta.T))
+        j = -(1 / self.m) * (self.Y.T.dot(np.log10(sigmoid(self.X.dot(self.theta.T))).T)+
+                     (
+                     (1 - self.Y).T.dot(np.log10(sigmoid(1 - (self.X.dot(self.theta.T)))).T))
+                     ) + (self.Lambda / 2 * self.m) * (self.theta.dot(self.theta.T))
 
         return j
 
-    def grad_descent(self):
+    def grad_descent(self, _Y, _theta):
         """ Gradient Descent with Regularization"""
-        theta = np.matrix(self.theta).T
+        _theta = np.matrix(_theta).T
 
-        for i in range(1, _iter + 1):
-            g = self.theta
+        for i in range(1, self.epoch + 1):
+            g = _theta
 
-            g[0, :] = g[0, :] - (self.alpha / m) * (
-                sigmoid(self.X.dot(g)) - self.Y[:, 0]).T.dot(self.X[:, 0])
+            g[0, :] = g[0, :] - (self.alpha / self.m) * (
+                sigmoid(self.X.dot(g)) - _Y[:, 0]).T.dot(self.X[:, 0])
 
             g[1:, ] = g[1:, ] * (1 - (self.alpha * self.Lambda / self.m)) - (
                 self.alpha / self.m) * (
-                self.X[:, 1:]).T.dot(sigmoid(self.X.dot(g)) - self.Y)
+                self.X[:, 1:]).T.dot(sigmoid(self.X.dot(g)) - _Y)
 
-            self.theta = g
-
-        return self.theta
+            _theta = g
+          
+        return _theta
 
     def fit(self):
-        # Training(Calculating Theta using Gradient Descent)
+        """Training (Calculating Theta using Gradient Descent) """
         for k in range(0, self.k):
-            theta[:, k] = descent(X, Y[:, k], self.theta[:, k], self.Lamb, self.alpha, self.epoch).T
+            theta[:, k] = self.grad_descent(
+                          X,
+                          Y[:, k],
+                          self.theta[:, k],
+                          ).T
 
-    def predict(self, X_test):
+    def predict(self, X_test, Y_act):
         """ Predicting the class of the Test Data"""
         Y_test = sigmoid(X_test.dot(self.theta))
         Y_test = (np.argmax(Y_test, axis=1))
         Y_class = list(map(lambda x: labels[x], Y_test))
+        Accuracy = np.sum(
+            np.matrix(list(map(lambda x, y: int(x == y), Y_test, Y_act)))
+            ) / Y_act.size * 100
+
+        return(Y_test, Y_class, Accuracy)
 
 
 # Miscellaneous functions
